b4df5b270666ae4f59ebbf20d55e3062346f1374
唐无忌 <tangwuji20040212@163.com>
2024-04-15 22:30:27 +0800

    测试打包

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/AnimalsWorldFront/',
+  base:'/animals-world-front/',
   plugins: [
     vue(),
     //导入Element+组件
